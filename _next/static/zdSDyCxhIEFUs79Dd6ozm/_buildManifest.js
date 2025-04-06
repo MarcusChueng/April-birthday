a644585f935ecd91c7a472017b4036da6dfe17f0
MarcusChueng <41632111+MarcusChueng@users.noreply.github.com>
2025-04-06 09:24:44 +0000

    Deploying to gh-pages from @ MarcusChueng/April-birthday@4b4eafeea146dbafb0c5756eaad1bf2332f7e6fe 🚀

---
+++ _next/static/zdSDyCxhIEFUs79Dd6ozm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/794-bc8a585d2b12fa14.js","static/css/bc34e4e88b057c26.css","static/chunks/pages/index-cea9993a90df9077.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
